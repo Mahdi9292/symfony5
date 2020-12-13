ff28711c0c9283c2abad4c0d4719a8a42ca30f7c
U-DESKTOP-GGQOBB5\drbit <mahdi.shaker92@gmail.com>
2020-12-13 12:34:14 +0100

    init

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,22 +4,34 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 { 
     /**
      * @return Response
      * @Route("/")
      */
-    
+    public function home(){
+        return new Response('welconme to home');
+    }
 
     /**
      * @return Response
      * @Route("/questions/{slug}")
      */
     public function show($slug){
-        return new Response($slug);
+
+        $answers = [
+            'there are no real name for earth',
+            'what are you talking about',
+            'it is hall malo konoha'
+        ];
+        return $this->render('question/show.html.twig', [
+            'question'=>ucwords(str_replace('-', ' ', $slug)),
+            'answers'=>$answers
+        ]);
     }
 }
